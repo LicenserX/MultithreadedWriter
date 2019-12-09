4ba6ed9a7f4d8fc964d5568a67fd3371b8273a3d
Dmitrii_Ananev <dmitrii_ananev@epam.com>
2019-12-09 17:39:23 +0300

    Task ready

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import writer.Writer;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
+    public static void main(String[] args) {
+        String relativePath = "1.txt";
+
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        executorService.submit(new Writer(relativePath, "Dimon"));
+        executorService.submit(new Writer(relativePath, "Darth Vader"));
+        executorService.submit(new Writer(relativePath, "Uncle Donald"));
+        executorService.submit(new Writer(relativePath, "Batman"));
+        executorService.shutdown();
+    }
+
 }
--- src/main/java/writer/Writer.java
+++ src/main/java/writer/Writer.java
@@ -1,4 +1,42 @@
 package writer;
 
-public class Writer {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Writer implements Runnable {
+
+    private final String path;
+
+    private final String message;
+
+    public Writer(String path, String message) {
+        this.path = path;
+        this.message = message;
+    }
+
+    @Override
+    public void run() {
+        write(path, message);
+    }
+
+    private static void write(String relativePath, String message) {
+        synchronized (Writer.class) {
+            String absolutePath = new File(relativePath).getAbsolutePath();
+
+            try (BufferedWriter bufferedWriter
+                         = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(absolutePath, true)))) {
+                int counter = 0;
+
+                while (counter < 5){
+                    bufferedWriter.write(counter++ + " " + message);
+                    bufferedWriter.write("\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
